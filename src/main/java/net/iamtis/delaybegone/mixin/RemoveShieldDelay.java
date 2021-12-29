dc815866528a7081a2337431c1433ca341407f7a
CovenantTurtle <47215321+CovenantTurtle@users.noreply.github.com>
2021-12-29 15:29:01 +1100

    Add files via upload

--- src/main/java/net/iamtis/delaybegone/mixin/RemoveShieldDelay.java
+++ src/main/java/net/iamtis/delaybegone/mixin/RemoveShieldDelay.java
@@ -6,9 +6,9 @@ import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(LivingEntity.class)
-public class RemoveShieldDelay {
+public abstract class RemoveShieldDelay {
 	@ModifyConstant(method = "isBlocking", constant = @Constant(intValue = 5))
-	private int removeDelay(int delay){
+	private int removeDelay(int delay) {
 		delay = 1;
 		return delay;
 	}
